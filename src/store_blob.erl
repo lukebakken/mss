fc558178a3b2a74e4d37b9fecd64a5e1a2c1ed93
Luke Bakken <luke@bakken.io>
2017-02-22 15:33:04 -0800

    POST is create-only, PUT is update-only

--- src/store_blob.erl
+++ src/store_blob.erl
@@ -9,22 +9,26 @@ store(#blob{id=Id}=Blob, Type) ->
     handle_get_filename(util:get_filename(Id), Blob, Type).
 
 handle_get_filename(FileName, #blob{id=Id}=Blob, post) ->
-    % post request allows create or update
+    % post request allows create only
     Bin = term_to_binary(Blob),
-    store_blob(post, Bin, Id, FileName);
+    handle_blob_exists(blob_exists(FileName), post, Bin, Id, FileName);
 handle_get_filename(FileName, #blob{id=Id}=Blob, put) ->
     % put request allows update only
     Bin = term_to_binary(Blob),
-    handle_blob_exists(blob_exists(FileName), Bin, Id, FileName).
+    handle_blob_exists(blob_exists(FileName), put, Bin, Id, FileName).
 
-handle_blob_exists({error, Reason}, _Bin, Id, _FileName) ->
+handle_blob_exists({error, Reason}, _Type, _Bin, Id, _FileName) ->
     Msg = io_lib:format("error|ID '~s': ~p", [Id, Reason]),
     {error, Msg};
-handle_blob_exists(false, _Bin, Id, _FileName) ->
+handle_blob_exists(false, post, Bin, Id, FileName) ->
+    store_blob(post, Bin, Id, FileName);
+handle_blob_exists(true, post, _Bin, Id, _FileName) ->
+    Msg = io_lib:format("error|blob with ID '~s' exists, use PUT to update.", [Id]),
+    {error, Msg};
+handle_blob_exists(false, put, _Bin, Id, _FileName) ->
     Msg = io_lib:format("error|blob with ID '~s' does not yet exist, use POST to create.", [Id]),
     {error, Msg};
-handle_blob_exists(true, Bin, Id, FileName) ->
-    % NB we know this code path can only come from a put
+handle_blob_exists(true, put, Bin, Id, FileName) ->
     store_blob(put, Bin, Id, FileName).
 
 store_blob(Type, Bin, Id, FileName) ->
